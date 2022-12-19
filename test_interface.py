2ab3e19376fecdc0abebb9ba96dbac4226277542
Arirou <sprozdut@gmail.com>
2022-12-19 16:53:16 +0100

    Changes by 33695

--- test_interface.py
+++ test_interface.py
@@ -1,24 +1,52 @@
 import pygame
 
+# Initialiser Pygame
 pygame.init()
 
-# Définition de la taille de la fenêtre
-window_width, window_height = 640, 480
+# Définir la taille de la fenêtre
+screen_width, screen_height = 640, 480
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Création de la fenêtre
-screen = pygame.display.set_mode((window_width, window_height))
+# Définir les couleurs
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+couleur = (39, 174, 173)
 
-# Chargement des images des boutons
-play_button_image = pygame.image.load("play_button.png")
-settings_button_image = pygame.image.load("settings_button.png")
-quit_button_image = pygame.image.load("quit_button.png")
-credits_button_image = pygame.image.load("credits_button.png")
+class Button:
+    def __init__(self, text, pos):
+        self.text = text
+        self.pos = pos
+        self.rect = pygame.Rect(pos, (150, 50))
 
-# Création des boutons
-play_button = pygame.Rect(50, 50, 100, 50)  # (x, y, largeur, hauteur)
-settings_button = pygame.Rect(50, 110, 100, 50)
-quit_button = pygame.Rect(50, 170, 100, 50)
-credits_button = pygame.Rect(50, 230, 100, 50)
+    def draw(self, screen):
+        # Dessiner le fond du bouton
+        pygame.draw.rect(screen, WHITE, self.rect)
+
+        # Dessiner le texte du bouton
+        text_surface = font.render(self.text, True, BLACK)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
+
+
+# Charger la police de caractères
+font = pygame.font.Font(None, 36)
+
+# Créer les boutons
+buttons = []
+buttons.append(Button("Jouer", (screen_width // 2 - 50, 100)))
+buttons.append(Button("Paramètres", (screen_width // 2 - 75, 150)))
+buttons.append(Button("Crédits", (screen_width // 2 - 50, 200)))
+buttons.append(Button("Quitter",  (screen_width // 2 - 60, 250)))
+
+
+# Calculer la position de chaque bouton
+button_width, button_height = 150, 50
+for i, button in enumerate(buttons):
+    button_x = (screen_width - button_width) // 2
+    button_y = 100 + i * 50
+    button_pos = (button_x, button_y)
+    button.pos = button_pos
+    button.rect = pygame.Rect(button_pos, (button_width, button_height))
 
 # Boucle principale
 running = True
@@ -26,29 +54,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        # Vérification des clics de souris
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if play_button.collidepoint(mouse_x, mouse_y):
-                # Code à exécuter lorsque le bouton "Jouer" est cliqué
-                pass
-            elif settings_button.collidepoint(mouse_x, mouse_y):
-                # Code à exécuter lorsque le bouton "Paramètres" est cliqué
-                pass
-            elif quit_button.collidepoint(mouse_x, mouse_y):
-                # Code à exécuter lorsque le bouton "Quitter" est cliqué
-                running = False
-            elif credits_button.collidepoint(mouse_x, mouse_y):
-                # Code à exécuter lorsque le bouton "Crédits" est cliqué
-                pass
-
-    # Dessin des boutons sur l'écran
-    screen.blit(play_button_image, play_button)
-    screen.blit(settings_button_image, settings_button)
-    screen.blit(quit_button_image, quit_button)
-    screen.blit(credits_button_image, credits_button)
-
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Vérifier si l'utilisateur a cliqué sur un bouton
+            for button in buttons:
+                if button.rect.collidepoint(event.pos):
+                    # Action à effectuer lorsque le bouton est cliqué
+                    if button.text == "Jouer":
+                        print("Jouer")
+                    elif button.text == "Paramètres":
+                        print("Paramètres")
+                    elif button.text == "Quitter":
+                        running = False
+                    elif button.text == "Crédits":
+                        print("Crédits")
+    # Dessiner la fenêtre
+    screen.fill(couleur)
+    for button in buttons:
+        button.draw(screen)
     pygame.display.flip()
 
+# Quitter Pygame
 pygame.quit()
